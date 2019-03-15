7a4d5b13c8bf13a85a66d9878c92fc5e94b6b3c5
Динар <45795277+dMau5@users.noreply.github.com>
2019-03-15 14:29:32 +0300

    Add files via upload

--- rew.py
+++ rew.py
@@ -11,7 +11,30 @@ from itertools import islice
 logging.basicConfig(level=logging.ERROR)
 
 
-# db = load_schema()
+# txt = 'US20040096399A1'
+# n = 0
+# with open('ex2.rdf', 'r', encoding='utf-8') as f:
+#     reactions = RDFread(f)
+#     for r in reactions:
+#         for reactant in r.reactants:
+#             _reactants = reactant.split()
+#             if any(sum(atom.charge for t, atom in molecule.atoms()) for molecule in _reactants):
+#                 pass
+#             else:
+#                 print('reactant', reactant)
+#                 print('_reactants', [str(x) for x in _reactants])
+#                 print('reagents', [str(x) for x in r.reagents])
+#         for product in r.products:
+#             _products = product.split()
+#             if any(sum(atom.charge for t, atom in molecule.atoms()) for molecule in _products):
+#                 pass
+#             else:
+#                 print('product', product)
+#                 print('_products', [str(x) for x in _products])
+#                 print('reagents', [str(x) for x in r.reagents])
+#
+# quit()
+
 # with open('up_graph.pickle', 'rb') as gr:
 #     print('load graph')
 #     Gr = pickle.load(gr)
@@ -27,27 +50,6 @@ logging.basicConfig(level=logging.ERROR)
 #     except: continue
 # with open('zinc.pickle', 'wb') as d:
 #     pickle.dump(zinc, d)
-# with SDFwrite('zinc_in_graph.sdf') as z:
-#     with db_session:
-#         for x in Gr.nodes():
-#             if 'zinc' in Gr.nodes[x]:
-#                 z.write(x)
-#
-# print('num')
-# for x in range(1, 13):
-#     with SDFread(f'zinc/{x}.sdf') as f1:
-#         print(x)
-#         for y in f1:
-#             num += 1
-#             if mol == y:
-#                 print('have')
-#         print('zinc', num)
-# num = 0
-# with db_session:
-#     for x in Gr.nodes():
-#         if 'zinc' in Gr.nodes[x]:
-#             num += 1
-# print('num', num)
 
 
 def worker(input_queue):
@@ -56,67 +58,51 @@ def worker(input_queue):
             r.standardize()
             with db_session:
                 for reactant in r.reactants:
-                    # if isinstance(reactant, MoleculeContainer):
                     _reactants = reactant.split()
-                    if any(sum(atom.charge for n, atom in molecule.atoms()) for molecule in _reactants):
+                    if all(sum(atom.charge for t, atom in molecule.atoms()) for molecule in _reactants) \
+                            or len(_reactants) == 1:
                         if not Molecule.structure_exists(reactant):
                             Molecule(reactant, User[1])
                     else:
+                        other = []
                         for molecule in _reactants:
+                            if not sum(atom.charge for t, atom in molecule.atoms()):
+                                if not Molecule.structure_exists(molecule):
+                                    Molecule(molecule, User[1])
+                            else:
+                                other.append(molecule)
+                        for mol in other:
+                            mol |= mol
+                        if not Molecule.structure_exists(mol):
+                            Molecule(mol, User[1])
+                
+                for product in r.products:
+                    _products = product.split()
+                    if any(sum(atom.charge for t, atom in molecule.atoms()) for molecule in _products):
+                        if not Molecule.structure_exists(product):
+                            Molecule(product, User[1])
+                    else:
+                        for molecule in _products:
                             if not Molecule.structure_exists(molecule):
                                 Molecule(molecule, User[1])
-                    # g.add_edge(reactant, n)
-                for product in r.products:
-                    if not Molecule.structure_exists(product):
-                        Molecule(product, User[1])
         except:
             continue
 
 
 if __name__ == '__main__':
     i = 0
-    db = load_schema('sandbox')
-    with open('reactions_db.rdf', 'r', encoding='utf-8') as f:
+    num = []
+    db = load_schema('sandbox',)
+    with open('final2.rdf', 'r', encoding='utf-8') as f:
         reactions = RDFread(f)
         print('work')
         # g = CGRdbDigraph()
         inp = Queue()
         for _ in range(12):
             Process(target=worker, args=(inp,)).start()
-        for x in reactions:
+        for x in islice(reactions, 200):
             print(f'--------{i} done--------')
             inp.put(x)
             i += 1
         for _ in range(12):
             inp.put('STOP')
-
-# with open('up_graph.pickle', 'wb') as f:
-#     pickle.dump(Gr, f)
-# print('finish')
-# with db_session:
-#     with open('zinc/log2.txt', 'w') as d:
-#         for node in Gr.nodes():
-#             if isinstance(node, int):
-#                 number = Gr.node[node]['reaxys_data'][0]
-#                 if number == bad:
-#                     d.write(f'node {node}\n')
-#                     d.write(f'bad {bad}\n')
-#                     d.write(f'predec {[str(x) for x in Gr._pred[node]]}\n')
-#                     d.write(f'synok {[str(x) for x in Gr._succ[node]]}\n')
-#                 elif number == bad1:
-#                     d.write(f'node {node}\n')
-#                     d.write(f'bad {bad1}\n')
-#                     d.write(f'predec {[str(x) for x in Gr._pred[node]]}\n')
-#                     d.write(f'synok {[str(x) for x in Gr._succ[node]]}\n')
-#                 else:
-#                     continue
-
-
-#             if txt not in text:
-#                 text.update({txt: n})
-#             else:
-#                 Gr.remove_node(node)
-#         else:
-#             continue
-
-
