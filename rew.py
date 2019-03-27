f8c931ec8313e478d7033db54e701becb319efeb
Динар <45795277+dMau5@users.noreply.github.com>
2019-03-27 13:37:51 +0300

    Add files via upload

--- rew.py
+++ rew.py
@@ -1,7 +1,8 @@
 import logging
 import pickle
 from pony.orm import db_session
-from CGRtools.files import SDFread, RDFread, SDFwrite
+from CGRtools.containers import ReactionContainer
+from CGRtools.files import SDFread, RDFread, SDFwrite, RDFwrite
 from digraph import CGRdbDigraph
 import networkx as nx
 from multiprocessing import Process, Queue
@@ -9,36 +10,39 @@ from CGRdbUser import User
 from CGRdb import load_schema, Molecule
 from itertools import islice
 logging.basicConfig(level=logging.ERROR)
+from CGRtools.algorithms import union
 
 
-# txt = 'US20040096399A1'
-# n = 0
-# with open('ex2.rdf', 'r', encoding='utf-8') as f:
-#     reactions = RDFread(f)
-#     for r in reactions:
-#         for reactant in r.reactants:
-#             _reactants = reactant.split()
-#             if any(sum(atom.charge for t, atom in molecule.atoms()) for molecule in _reactants):
-#                 pass
-#             else:
-#                 print('reactant', reactant)
-#                 print('_reactants', [str(x) for x in _reactants])
-#                 print('reagents', [str(x) for x in r.reagents])
-#         for product in r.products:
-#             _products = product.split()
-#             if any(sum(atom.charge for t, atom in molecule.atoms()) for molecule in _products):
-#                 pass
-#             else:
-#                 print('product', product)
-#                 print('_products', [str(x) for x in _products])
-#                 print('reagents', [str(x) for x in r.reagents])
-#
-# quit()
-
-# with open('up_graph.pickle', 'rb') as gr:
+db = load_schema('sandbox',)
+w = SDFwrite('exists.sdf')
+we = RDFwrite('similar.rdf')
+for x in range(1872):
+    try:
+        f = SDFread(f'zinc/{x}.sdf')
+        for z_mol in f:
+            z_mol.aromatize()
+            if Molecule.structure_exists(z_mol):
+                w.write(z_mol)
+            else:
+                mols = Molecule.find_similar(z_mol)
+                mols = [x[0].structure for x in mols if x[-1] >= 0.95]
+                r = ReactionContainer()
+                r.reactants = [z_mol]
+                r.products = mols
+                we.write(r)
+    except:
+        continue
+# with open('2_5_graph.pickle', 'rb') as gr:
 #     print('load graph')
 #     Gr = pickle.load(gr)
 #     print('graph loaded')
+# n = 0
+# with db_session:
+#     for node in Gr.nodes():
+#         if not isinstance(node, int):
+#             if 'zinc' in node:
+#                 n += 1
+# print('n', n)
 # zinc = set()
 # for x in range(1, 1872):
 #     try:
@@ -52,57 +56,75 @@ logging.basicConfig(level=logging.ERROR)
 #     pickle.dump(zinc, d)
 
 
-def worker(input_queue):
-    for r in iter(input_queue.get, 'STOP'):
-        try:
-            r.standardize()
-            with db_session:
-                for reactant in r.reactants:
-                    _reactants = reactant.split()
-                    if all(sum(atom.charge for t, atom in molecule.atoms()) for molecule in _reactants) \
-                            or len(_reactants) == 1:
-                        if not Molecule.structure_exists(reactant):
-                            Molecule(reactant, User[1])
-                    else:
-                        other = []
-                        for molecule in _reactants:
-                            if not sum(atom.charge for t, atom in molecule.atoms()):
-                                if not Molecule.structure_exists(molecule):
-                                    Molecule(molecule, User[1])
-                            else:
-                                other.append(molecule)
-                        for mol in other:
-                            mol |= mol
-                        if not Molecule.structure_exists(mol):
-                            Molecule(mol, User[1])
-                
-                for product in r.products:
-                    _products = product.split()
-                    if any(sum(atom.charge for t, atom in molecule.atoms()) for molecule in _products):
-                        if not Molecule.structure_exists(product):
-                            Molecule(product, User[1])
-                    else:
-                        for molecule in _products:
-                            if not Molecule.structure_exists(molecule):
-                                Molecule(molecule, User[1])
-        except:
-            continue
+# def worker(input_queue):
+#     for r in iter(input_queue.get, 'STOP'):
+#         try:
+#             r.standardize()
+#             with db_session:
+#                 for reactant in r.reactants:
+                    # _reactants = reactant.split()
+                    # if all(sum(atom.charge for t, atom in molecule.atoms()) for molecule in _reactants) \
+                    #         or len(_reactants) == 1:
+                    # if not Molecule.structure_exists(reactant):
+                    #     Molecule(reactant, User[1])
+                    # else:
+                    #     other = []
+                    #     for molecule in _reactants:
+                    #         if not sum(atom.charge for t, atom in molecule.atoms()):
+                    #             if not Molecule.structure_exists(molecule):
+                    #                 Molecule(molecule, User[1])
+                    #         else:
+                    #             other.append(molecule)
+                    #     er = other.pop()
+                    #     if len(other) > 1:
+                    #         for mol in other:
+                    #             er |= mol
+                    #     if not Molecule.structure_exists(er):
+                    #         Molecule(er, User[1])
+
+                # for product in r.products:
+                    # _products = product.split()
+                    # if all(sum(atom.charge for t, atom in molecule.atoms()) for molecule in _products) \
+                    #         or len(_products) == 1:
+                    # if not Molecule.structure_exists(product):
+                    #     Molecule(product, User[1])
+                    # else:
+                    #     other = []
+                    #     for molecule in _products:
+                    #         if not sum(atom.charge for t, atom in molecule.atoms()):
+                    #             if not Molecule.structure_exists(molecule):
+                    #                 Molecule(molecule, User[1])
+                    #         else:
+                    #             other.append(molecule)
+                    #     er = other.pop()
+                    #     if len(other) > 1:
+                    #         for mol in other:
+                    #             er |= mol
+                    #     if not Molecule.structure_exists(er):
+                    #         Molecule(er, User[1])
+        # except:
+        #     continue
 
 
-if __name__ == '__main__':
-    i = 0
-    num = []
-    db = load_schema('sandbox',)
-    with open('final2.rdf', 'r', encoding='utf-8') as f:
-        reactions = RDFread(f)
-        print('work')
-        # g = CGRdbDigraph()
-        inp = Queue()
-        for _ in range(12):
-            Process(target=worker, args=(inp,)).start()
-        for x in islice(reactions, 200):
-            print(f'--------{i} done--------')
-            inp.put(x)
-            i += 1
-        for _ in range(12):
-            inp.put('STOP')
+# if __name__ == '__main__':
+#     i = 1000000
+#     num = []
+#     db = load_schema('old',)
+#     with open('reactions_db.rdf', 'r', encoding='utf-8') as f:
+#         print('init')
+#         reactions = RDFread('reactions_db.rdf', indexable=True)
+#         print('start seek')
+#         reactions.seek(1000000)
+#         print('len', len(reactions))
+#         print('work')
+#         # g = CGRdbDigraph()
+#         inp = Queue()
+#         for _ in range(12):
+#             Process(target=worker, args=(inp,)).start()
+#         for x in reactions:
+#             if not i % 1000:
+#                 print(f'--------{i} done--------')
+#             inp.put(x)
+#             i += 1
+#         for _ in range(12):
+#             inp.put('STOP')
