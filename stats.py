740b75e2dd451826187813c772462aa6a4972f73
Динар <45795277+dMau5@users.noreply.github.com>
2019-06-01 13:21:19 +0300

    Add files via upload

--- stats.py
+++ stats.py
@@ -6,6 +6,7 @@ from pony.orm import db_session
 from pickle import load, dump
 from watch import paths_of_synthesis_for_target_molecule
 from watch import visualization
+from networkx.algorithms.cycles import simple_cycles
 
 load_schema('all_patents', )
 with open('zinc.pickle', 'rb') as z:
@@ -23,6 +24,9 @@ with SDFread('drugs_in_patents_as_product.sdf') as d:
                 drug_in_db = Molecule.find_structure(drug)
                 g1 = paths_of_synthesis_for_target_molecule(drug_in_db, 2)
                 g = g1.copy()
+                # synky = g1._succ[drug]
+                # if synky:
+                #     g.remove_nodes_from(synky)
                 st = [x for x in g1.nodes() if not isinstance(x, int) and bytes(x) not in zinc]
                 while True:
                     for n, structure in enumerate(st):
@@ -34,16 +38,20 @@ with SDFread('drugs_in_patents_as_product.sdf') as d:
                             break
                     else:
                         break
+
                 bonds = g._adj
                 seen = {drug}
                 stack = [drug]
                 while stack:
                     mt = stack.pop(0)
-                    for i in bonds[mt].keys() - seen:
-                        stack.append(i)
-                        seen.add(i)
-                t = visualization(g, drug)
-                r = 6
+                    if mt in bonds:
+                        for i in bonds[mt].keys() - seen:
+                            stack.append(i)
+                            seen.add(i)
+                viz = g.subgraph(seen)
+                if len(viz) > 1:
+                    t = visualization(viz, drug)
+                    r = 6
 
 with open('stats.pickle', 'wb') as p:
     dump(drugs_in_reactions, p)
