b02935847beaeceeb74cc9d719e01323aa6eb635
Динар <45795277+dMau5@users.noreply.github.com>
2019-08-26 18:07:52 +0300

    Add files via upload

--- for_db.py
+++ for_db.py
@@ -5,13 +5,14 @@ from CGRtools.files import RDFwrite
 from CGRtools.containers import ReactionContainer
 import numpy as np
 from math import ceil
+from multiprocessing import Queue, Process
 from pony.orm import select, db_session
 from pickle import dump, load
 from itertools import islice, cycle, filterfalse
 from random import randint
 from time import time
 
-load_schema('all_patents', )
+load_schema('all_patents',)
 
 
 def evaluation(query, res):
@@ -101,8 +102,8 @@ def roundrobin2(iterables):
     dt = []
     for tu_sh, tu in iterables:
         try:
-            dt.append((tu_sh, tu))
             yield next(tu_sh), tu
+            dt.append((tu_sh, tu))
         except StopIteration:
             pass
 
@@ -135,47 +136,61 @@ def unique_everseen(iterable, key=None):
                 yield element
 
 
-with RDFwrite('False_pairs_.rdf') as fw:
-    for i, mol_id_1 in enumerate(sigma):
-        print(f'-- {i} powel --')
-        new_id = old_to_new[mol_id_1]
-        with db_session:
-            tshki_1 = pairs[mol_id_1]
-            total = len(tshki_1)
-            print('total', total)
-            ini = time()
-            t_sh01 = ((iter(pairs[new_dict[n]] - tshki_1), tshki_1 - pairs[new_dict[n]])
-                      for n, x in enumerate(big[new_id]) if x <= 25.5 and new_id != n)
-            t_sh45 = ((iter(pairs[new_dict[n]] - tshki_1), tshki_1 - pairs[new_dict[n]])
-                      for n, x in enumerate(big[new_id]) if 102 <= x <= 127.5 and new_id != n)
-            t_sh90 = ((iter(pairs[new_dict[n]] - tshki_1), tshki_1 - pairs[new_dict[n]])
-                      for n, x in enumerate(big[new_id]) if x >= 229.5 and new_id != n)
-            # print('ini', time())
-            # rr1 = roundrobin1(t_sh01, t_sh45, t_sh90)
-            # print('rr1', time())
-            # rr2 = roundrobin2(rr1)
-            # print('rr2', time())
-            # uee = unique_everseen(rr2, lambda x: x[0])
-            # print('uee', time())
-            # exit()
-            n = 0
-            for ts, t in islice(unique_everseen(roundrobin2(roundrobin1(t_sh01, t_sh45, t_sh90)), lambda x: x[0]),
-                                total):
-                n += 1
-                with open(f'database/{ts}.pickle', 'rb') as p1:
-                    fp_1 = load(p1)
-                ind = 0
-                for ii in t:
-                    with open(f'database/{ii}.pickle', 'rb') as p2:
-                        fp_2 = load(p2)
-                    new_ind = evaluation(fp_1, fp_2)
-                    if new_ind > ind:
-                        ind = new_ind
-                if ind:
+def worker(p):
+    tsh, tu = p.get()
+    with open(f'database/{tsh}.pickle', 'rb') as p1:
+        fp_1 = load(p1)
+    index = 0
+    for ii in tu:
+        with open(f'database/{ii}.pickle', 'rb') as p2:
+            fp_2 = load(p2)
+        new_ind = evaluation(fp_1, fp_2)
+        if new_ind > index:
+            index = new_ind
+    if index:
+        out.put((tsh, index))
+
+
+if __name__ == '__main__':
+    with RDFwrite('False_pairs.rdf') as fw:
+        inp = Queue()
+        out = Queue()
+        for _ in range(20):
+            Process(target=worker, args=(inp, )).start()
+
+        for i, mol_id_1 in enumerate(sigma):
+            print(f'-- {i} powel --')
+            # if i == 3:
+            #     exit()
+            new_id = old_to_new[mol_id_1]
+            with db_session:
+                tshki_1 = pairs[mol_id_1]
+                total = len(tshki_1)
+                print('total', total)
+                ini = time()
+                t_sh01 = ((iter(pairs[new_dict[n]] - tshki_1), tshki_1 - pairs[new_dict[n]])
+                          for n, x in enumerate(big[new_id]) if x <= 25.5 and new_id != n)
+                t_sh45 = ((iter(pairs[new_dict[n]] - tshki_1), tshki_1 - pairs[new_dict[n]])
+                          for n, x in enumerate(big[new_id]) if 102 <= x <= 127.5 and new_id != n)
+                t_sh90 = ((iter(pairs[new_dict[n]] - tshki_1), tshki_1 - pairs[new_dict[n]])
+                          for n, x in enumerate(big[new_id]) if x >= 229.5 and new_id != n)
+                # print('ini', time())
+                # rr1 = roundrobin1(t_sh01, t_sh45, t_sh90)
+                # print('rr1', time())
+                # rr2 = roundrobin2(rr1)
+                # print('rr2', time())
+                # uee = unique_everseen(rr2, lambda x: x[0])
+                # print('uee', time())
+                # exit()
+                n = 0
+                for pair in islice(unique_everseen(roundrobin2(roundrobin1(t_sh01, t_sh45, t_sh90)), lambda x: x[0]),
+                                   total):
+                    inp.put(pair)
+                for _ in range(total):
+                    ts, ind = out.get()
                     fw.write(ReactionContainer(reactants=[Molecule[mol_id_1].structure],
                                                products=[Molecule[ts].structure], meta={'fer': ind / 2}))
-            print('prowlo', time() - ini, 'sec')
-            print('usego', n)
+                print('prowlo', time() - ini, 'sec')
 
             # try:
             #     tshki_2_h = pairs[high_mol_id]
